0631dcda8878b6a1c7a1a61a1c4023c995df746d
Eduardo377 <eduardogomes377@gmail.com>
2023-08-31 05:00:30 -0300

    :build: feat: header gerado dinamicamente pelo o JS, prepando build para subir pra deploy

--- script/footer-script.js
+++ script/footer-script.js
@@ -1,4 +1,4 @@
-const footer = document.querySelector("footer");
+const dynamicFooter = document.getElementById('footer');
 
 const footerContainer = document.createElement("section");
 footerContainer.classList.add("footer-container");
@@ -18,18 +18,46 @@ lgpdText.innerText = "Lei Geral de Proteção de Dados Pessoais (LGPD), Lei n° 
 const footerSocial = document.createElement("div");
 footerSocial.classList.add("footer-social");
 
-const footerSocialConteiner = document.createElement("div");
+const footerSocialConteiner = document.createElement('div');
 
 const socialText = document.createElement("p");
 socialText.innerText = "Nos siga nas redes sociais";
+
+const socialIcons = document.createElement('div');
+
+
+const socialLinks = [
+    { name: 'WhatsApp', icon: 'whatsapp-logo.png', link: 'https://www.whatsapp.com/?lang=pt_BR' },
+    { name: 'Twitter', icon: 'twitter-logo.png', link: 'https://twitter.com' },
+    { name: 'YouTube', icon: 'youtube-logo.png', link: 'https://www.youtube.com/' },
+    { name: 'Pinterest', icon: 'pinterest-logo.png', link: 'https://br.pinterest.com/' },
+    { name: 'Instagram', icon: 'instagram-logo.png', link: 'https://br.pinterest.com/' },
+    { name: 'Facebook', icon: 'facebook-logo.png', link: 'https://www.facebook.com/' },
+];
+
+socialLinks.forEach((social) => {
+    const socialLink = document.createElement('a');
+    socialLink.href = social.link;
+    socialLink.target = "_blank";
+
+    const socialImg = document.createElement('img');
+    socialImg.className = 'social';
+    socialImg.src = `../assets/${social.icon}`;
+    socialImg.alt = social.name;
+
+    socialLink.appendChild(socialImg);
+    socialIcons.appendChild(socialLink);
+});
 
 footerSocialConteiner.appendChild(socialText);
 
 footerSocial.appendChild(footerSocialConteiner);
 
+footerSocial.appendChild(socialIcons);
+footerInfo.appendChild(lgpdLink);
+lgpdLink.appendChild(lgpdText);
+
+footerContainer.appendChild(footerInfo);
 footerContainer.appendChild(footerSocial);
 
-lgpdLink.appendChild(lgpdText);
-footerInfo.appendChild(lgpdLink);
-footerContainer.appendChild(footerInfo);
-footer.appendChild(footerContainer)
+dynamicFooter.appendChild(footerContainer);
